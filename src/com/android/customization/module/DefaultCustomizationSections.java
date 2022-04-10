0d7802bed2866a3649b19e01c03b68216d3b0b6b
jhonboy121 <alfredmathew05@gmail.com>
2022-04-11 02:07:34 +0530

    ThemePicker: add support for custom themed icon pack [2/3]
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- src/com/android/customization/module/DefaultCustomizationSections.java
+++ src/com/android/customization/module/DefaultCustomizationSections.java
@@ -16,6 +16,7 @@ import com.android.customization.model.iconpack.IconPackManager;
 import com.android.customization.model.iconpack.IconPackSectionController;
 import com.android.customization.model.mode.DarkModeSectionController;
 import com.android.customization.model.theme.OverlayManagerCompat;
+import com.android.customization.model.themedicon.ThemedIconPackSectionController;
 import com.android.customization.model.themedicon.ThemedIconSectionController;
 import com.android.customization.model.themedicon.ThemedIconSwitchProvider;
 import com.android.customization.picker.quickaffordance.domain.interactor.KeyguardQuickAffordancePickerInteractor;
@@ -125,7 +126,7 @@ public final class DefaultCustomizationSections implements CustomizationSections
             CustomizationSectionNavigationController sectionNavigationController,
             @Nullable Bundle savedInstanceState,
             DisplayUtils displayUtils) {
-        List<CustomizationSectionController<?>> sectionControllers = new ArrayList<>();
+        final List<CustomizationSectionController<?>> sectionControllers = new ArrayList<>();
 
         // Wallpaper section.
         sectionControllers.add(new WallpaperSectionController(
@@ -141,6 +142,12 @@ public final class DefaultCustomizationSections implements CustomizationSections
         sectionControllers.add(new ThemedIconSectionController(
                 ThemedIconSwitchProvider.getInstance(activity), workspaceViewModel,
                 savedInstanceState));
+
+        // Custom themed icon pack section.
+        sectionControllers.add(new ThemedIconPackSectionController(
+                activity, sectionNavigationController,
+                ThemedIconSwitchProvider.getInstance(activity),
+                lifecycleOwner, savedInstanceState));
 
         // App grid section.
         sectionControllers.add(new GridSectionController(
